3bc02df5b4ab6bf3d968473a5d96381a93a11af7
Tamizhvendan S <tamizh88@gmail.com>
2012-12-08 01:02:32 -0600

    Adding Login Screen

---
+++ Gameo.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Gameo.Web.ViewModels;
+
+namespace Gameo.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        public ViewResult Login()
+        {
+            var loginViewModel = new LoginViewModel
+                                     {
+                                        UserName = "", Password = "", 
+                                        BranchSelectListItems = new[]
+                                        {
+                                            new SelectListItem{ Selected = true, Text = "Select a branch", Value = "0" },
+                                            new SelectListItem {Selected = true, Text = "T.Nagar", Value = "1"}, 
+                                            new SelectListItem {Text = "Virugambakam", Value = "2"}
+                                        }
+                                     };
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ Gameo.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Gameo.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int BranchId { get; set; }
+        public IEnumerable<SelectListItem> BranchSelectListItems { get; set; }
+    }
+}
